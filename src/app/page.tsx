d9d34f83d3ab83c19a45b38467cc71cd26088d62
farukulWD <farukgb1999@gmail.com>
2025-09-05 01:26:22 +0600

    Add Tailwind CSS configuration and utility functions; implement Button component and update Home page JP-8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-
-
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex bg-beige flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl text-brand-subheading font-bold">
+        Grow Your Audience High-Quality Ambience & Meditation Videos
+      </h1>
+    </div>
+  );
 }
