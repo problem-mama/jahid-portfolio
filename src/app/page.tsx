1e9c9f6807527fedcc80687fa60a579ab9c91965
farukulWD <farukgb1999@gmail.com>
2025-10-03 21:34:49 +0600

    feat: add Contact, Testimonial, and FAQ sections to Home page; integrate Swiper for testimonials and update styles for navigation buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import AboutSection from "@/components/home/about-section/about-section";
 import HeroSection from "@/components/home/hero-section";
 import PortfolioSection from "@/components/home/portfolio/portfolio-section";
 import ServiceSection from "@/components/home/service-section/service-section";
+import TestimonialSection from "@/components/home/testimonial-section/testimonial-section";
+import FaqSection from "@/components/home/faq-section/faq-section";
+import ContactSection from "@/components/home/contact-section/contact-section";
 
 export default function Home() {
   return (
@@ -17,6 +20,15 @@ export default function Home() {
       </section>
       <section>
         <ServiceSection />
+      </section>
+      <section>
+        <TestimonialSection />
+      </section>
+      <section>
+        <ContactSection />
+      </section>
+      <section>
+        <FaqSection />
       </section>
     </main>
   );
