1c8e0a0f223848a4a73978fb783e96b969e8f1c3
farukulWD <121636449+farukulWD@users.noreply.github.com>
2025-11-05 00:22:51 +0600

    feat: replace Roboto with Poppins font for improved typography consistency (#12)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import MainLayout from "@/layouts/main-layouts";
-import { Roboto } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const roboto = Roboto({
-  weight: ["400", "500", "600", "700"],
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
-  variable: "--font-roboto",
+  variable: "--font-poppins",
 });
+
 // here you can add metadata for your application
 export const metadata: Metadata = {
   title: "Jahid visuals",
@@ -22,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={` ${roboto.variable} antialiased bg-background text-foreground`}
+        className={`${poppins.variable} antialiased bg-background text-foreground`}
       >
         <MainLayout>{children}</MainLayout>
       </body>
